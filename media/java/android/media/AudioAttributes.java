Superior-beta/android_frameworks_base

commit 980e58c7183e325bcc41098b9a652a3ad8c7854b

AudioAttributes: Fix Builder.setFlags()
AudioAttributes.Builder.setFlags() is a public method and should only
allow to set public flags

Test: CTS AudioAttributesTest
Change-Id: Ib75e3487f5297f6a200ffe326c53a0f58458b51f

media/java/android/media/AudioAttributes.java
@@ -710,7 +710,7 @@ public final class AudioAttributes implements Parcelable {
          * @return the same Builder instance.
          */
         public Builder setFlags(int flags) {
-            flags &= AudioAttributes.FLAG_ALL;
+            flags &= AudioAttributes.FLAG_ALL_PUBLIC;
             mFlags |= flags;
             return this;
         }

commit 195d40c4ec8395c4308ad326e0aa174a0b4b58c0

AudioAttributes: fix setter for flags
A regression was introduced by 980e58c which prevented @SystemApi
flags to be set in the builder of an AudioAttributes, causing
FLAG_BYPASS_INTERRUPTION_POLICY and FLAG_BYPASS_MUTE to get
ignored.
Note that even though any client can set System flags, they will
be discarded and ignored server side in the audio policy service if
the caller doesn't have the permissions.

Bug: 154205921
Test: fire alerts with packages/apps/CellBroadcastReceiver/tests/testapp
Change-Id: I9d593787c18a510aa702dc0da85254f0d9574f11

media/java/android/media/AudioAttributes.java
@@ -452,6 +452,10 @@ public final class AudioAttributes implements Parcelable {
             | FLAG_NO_SYSTEM_CAPTURE | FLAG_CAPTURE_PRIVATE;
     private final static int FLAG_ALL_PUBLIC = FLAG_AUDIBILITY_ENFORCED |
             FLAG_HW_AV_SYNC | FLAG_LOW_LATENCY;
+    /* mask of flags that can be set by SDK and System APIs through the Builder */
+    private static final int FLAG_ALL_API_SET = FLAG_ALL_PUBLIC
+            | FLAG_BYPASS_INTERRUPTION_POLICY
+            | FLAG_BYPASS_MUTE;
 
     /**
      * Indicates that the audio may be captured by any app.
@@ -838,7 +842,7 @@ public final class AudioAttributes implements Parcelable {
          * @return the same Builder instance.
          */
         public Builder setFlags(int flags) {
-            flags &= AudioAttributes.FLAG_ALL_PUBLIC;
+            flags &= AudioAttributes.FLAG_ALL_API_SET;
             mFlags |= flags;
             return this;
         }